8a02e5ac81790d4ec114f03124a49bbed2244071
Selva <selvarmd@gmail.com>
2025-09-13 13:56:54 +0530

    sticky header added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,12 @@
 // Header
-// window.addEventListener("scroll", function () {
-//   const header = document.querySelector("header");
-//   if (window.scrollY > 0) {
-//     header.classList.add("scrolled");
-//   } else {
-//     header.classList.remove("scrolled");
-//   }
-// });
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
 
 // Logo carousel
 (function () {
